7436a8cc07a1da14ae7170c1da8c393d19a8f0c0
tanya-al <talrehan@ucla.edu>
2017-04-01 18:31:38 -0700

    my version of everything

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('.'));
+
 var mongoose = require('mongoose');
 
 var mongodbUri = 'mongodb://mylipdb:myl1pdb!lahacks@ds060649.mlab.com:60649/mylipdb';
@@ -25,9 +30,6 @@ var ProfileInfo = new Schema ({
 
 var myModel = mongoose.model('ProfileModel', ProfileInfo);
 
-app.use(express.static('.'));
-app.use(bodyParser.urlencoded({extended: false}));
-
 app.get('/',function(req,res) {
         res.sendFile("html/index.html",{"root":__dirname});
 });
@@ -41,21 +43,27 @@ app.get('/signup',function(req,res) {
     });
 
 app.post('/login',function(req,res) {
-	var data = req.body;
-	
+	//var data = req.body;
+	res.send('Not implemented yet');
     });
   
-app.post('/signup',function(req,res) {
-	var data = req.body;
-        console.log(data);
+app.post('/signup',bodyParser,function(req,res) {
+	/*var data = req.body;
+        res = {
+	    username:data.username,
+	    password:data.password,
+	    email:data.email
+	};
+	console.log(res);*/
+	res.send('Done');//JSON.stringify(res));
 	/*myModel.create({username: data.username, password: data.password},function(err,instance) {
 		if(err) return handleError(err);
 		});*/
-	    res.send('Account Created');
+	//res.send('Account Created');
     });
 
 var server = app.listen(8081, function () {
-      var host = server.address().address
+     var host = server.address().address
      var port = server.address().port
   
     console.log("Example app listening at http://%s:%s", host, port)
